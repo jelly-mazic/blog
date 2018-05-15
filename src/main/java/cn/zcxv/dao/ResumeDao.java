d5035c1ea4bd49f928e905989ce6bbcbd4043710
e <tomazic@qq.com>
2018-05-15 21:21:14 +0800

    resumeDao层开发测试完成

---
+++ src/main/java/cn/zcxv/dao/ResumeDao.java
@@ -0,0 +1,27 @@
+package cn.zcxv.dao;
+
+import cn.zcxv.entity.Resume;
+import org.apache.ibatis.annotations.Param;
+
+
+public interface ResumeDao {
+    /**
+     * 查询简历内容
+     *
+     */
+    Resume queryResume(@Param("resumeId") int resumeId);
+
+    /**
+     * 更新简历
+     * @return 如果影响行数>0,代表更新成功
+     */
+    int updateResume(@Param("updateResume") Resume updateResume);
+
+    /**
+     * 插入实体类
+     * @param selfResume
+     */
+    void insertResume(@Param("selfResume") Resume selfResume);
+
+
+}
---
+++ src/main/test/ResumeDaoTest.java
@@ -0,0 +1,56 @@
+import cn.zcxv.dao.ResumeDao;
+import cn.zcxv.entity.Resume;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by codingBoy on 16/11/27.
+ * 配置spring和junit整合，这样junit在启动时就会加载spring容器
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring的配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class ResumeDaoTest {
+    @Resource
+    private ResumeDao resumeDao;
+
+    @Test
+    public void queryById() throws Exception {
+        int id = 1;
+        Resume result = resumeDao.queryResume(id);
+        System.out.println(result.getSelfIntroduce());
+    }
+
+    @Test
+    public void saveResume() {
+        Resume r = new Resume();
+        r.setSelfAbility("12345645");
+        r.setSelfEducation("123");
+        r.setSelfExperience("er");
+        r.setSelfIntroduce("er");
+        r.setSelfLanguage("er");
+        r.setSelfHobby("er");
+        resumeDao.insertResume(r);
+    }
+
+    @Test
+    public void changResume() {
+        Resume r = new Resume();
+        r.setSelfAbility("12345645");
+        r.setSelfEducation("123");
+        r.setSelfExperience("er");
+        r.setSelfIntroduce("er123");
+        r.setSelfLanguage("er");
+        r.setSelfHobby("er");
+        r.setResumeId(1);
+        resumeDao.updateResume(r);
+
+    }
+
+
+}
+
